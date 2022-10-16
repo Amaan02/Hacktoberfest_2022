ac069a226ba38dccc2286d8215bc540c5eaffe6e
Amaan02 <73248384+Amaan02@users.noreply.github.com>
2022-10-16 21:38:32 +0530

    Add files via upload

---
+++ Push_Pop_Size_Empty.cpp
@@ -0,0 +1,105 @@
+#include<iostream>
+#include<stack>
+using namespace std;
+
+class Stack{
+    public: 
+        int *arr;
+        int top;
+        int size;
+
+    Stack(int size){
+        this->size = size;
+        arr = new int[size];
+        top = -1;
+    }
+
+    void push(int element){
+        if(size-top>1){
+            top++;
+            arr[top] = element;
+        }else{
+            cout<<"Stack Overflow"<<endl;
+        }
+    }
+
+    void pop(){
+        if(top>=0){
+            top--;
+        }else{
+            cout<<"Stack Underflow"<<endl;
+        }
+    }
+
+    int peek(){
+        if(top>=0){
+            return arr[top];
+        }else{
+            cout<<"Stack is Empty"<<endl;
+            return -1;
+        }
+    }
+
+    bool isEmpty(){
+        if(top==-1){
+            return true;
+        }else{
+            return false;
+        }
+    }
+};
+
+int main(){
+    Stack st(5);
+    st.push(10);
+    st.push(20);
+    st.push(30);
+    st.push(40);
+    st.push(50);
+    
+    cout<<st.peek()<<endl;
+
+    st.pop();
+    st.pop();
+    st.pop();
+
+    cout<<st.peek()<<endl;
+
+    if(st.isEmpty()){
+        cout<<"Stack is Empty"<<endl;
+    }else{
+        cout<<"Stack is not Empty"<<endl;
+    }
+}
+
+/*
+int main(){
+    stack<int> s;
+    s.push(1);
+    s.push(2);
+    s.push(3);
+    s.push(4);
+
+    cout<<s.top()<<endl;
+
+    s.pop();
+    cout<<s.top()<<endl;
+
+    s.pop();
+    cout<<s.top()<<endl;
+
+    s.pop();
+    cout<<s.top()<<endl;
+
+    //size 
+    cout<<s.size()<<endl;
+
+    //empty
+    if(s.empty()){
+        cout<<"Stack is empty"<<endl;
+    }
+    else{
+        cout<<"Stack is not empty"<<endl;
+    }
+}
+*/
